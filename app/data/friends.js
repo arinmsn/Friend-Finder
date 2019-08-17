8704216c1aab4ea41c218f4dc78cef2c2540dfb9
Arin M <arinmsn@gmail.com>
2019-08-16 21:34:55 -0700

    Minor code update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,73 +1,28 @@
 friends = [
-    {
-        name: 'Frasier Crane',
-        photo: 'https://vignette.wikia.nocookie.net/frasier/images/b/b7/Frasiercrane.jpeg/revision/latest?cb=20180115011532',
-        scores: [
-            '4',
-            '4',
-            '3',
-            '3',
-            '2',
-            '3',
-            '3',
-            '2',
-            '4',
-            '4'
-        ]
-    },
-    {
-        name: 'Abe Simpson',
-        photo: 'https://www.lawgarcia.com/wp-content/uploads/2015/03/grandpa-simpson-300x218.jpg',
-        scores: [
-            '5',
-            '5',
-            '5',
-            '1',
-            '1',
-            '1',
-            '2',
-            '2',
-            '3',
-            '3'
-        ]
-    },
-    {
-        name: 'Oscar the Grouch',
-        photo: 'https://vignette.wikia.nocookie.net/muppet/images/5/5f/OscarsClassicStare.jpg/revision/latest?cb=20120222062511',
-        scores: [
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1'
-        ]
-    },
-    {
-        name: 'IU',
-        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/IU_at_%27dlwlrma%27_concert_in_Jeju_on_January_5%2C_2019_%283%29.jpg/330px-IU_at_%27dlwlrma%27_concert_in_Jeju_on_January_5%2C_2019_%283%29.jpg',
-        scores: [
-            '4',
-            '5',
-            '4',
-            '5',
-            '4',
-            '5',
-            '3',
-            '3',
-            '3',
-            '3'
-        ]
-    }
-
-
-
-
-
-]
+  {
+    name: "Frasier Crane",
+    photo:
+      "https://vignette.wikia.nocookie.net/frasier/images/b/b7/Frasiercrane.jpeg/revision/latest?cb=20180115011532",
+    scores: ["4", "4", "3", "3", "2", "3", "3", "2", "4", "4"]
+  },
+  {
+    name: "Abe Simpson",
+    photo:
+      "https://www.lawgarcia.com/wp-content/uploads/2015/03/grandpa-simpson-300x218.jpg",
+    scores: ["5", "5", "5", "1", "1", "1", "2", "2", "3", "3"]
+  },
+  {
+    name: "Oscar the Grouch",
+    photo:
+      "https://vignette.wikia.nocookie.net/muppet/images/5/5f/OscarsClassicStare.jpg/revision/latest?cb=20120222062511",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  },
+  {
+    name: "IU",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/IU_at_%27dlwlrma%27_concert_in_Jeju_on_January_5%2C_2019_%283%29.jpg/330px-IU_at_%27dlwlrma%27_concert_in_Jeju_on_January_5%2C_2019_%283%29.jpg",
+    scores: ["4", "5", "4", "5", "4", "5", "3", "3", "3", "3"]
+  }
+];
 
 module.exports = friends;
