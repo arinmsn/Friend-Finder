ccae651e6b44923d8df5a3d9ca92d36a5780c6d9
Arin M <arinmsn@gmail.com>
2019-08-17 18:14:17 -0700

    Cleaned up JSON - will replace links to local images

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,12 +16,6 @@ friends = [
     photo:
       "https://vignette.wikia.nocookie.net/muppet/images/5/5f/OscarsClassicStare.jpg/revision/latest?cb=20120222062511",
     scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
-  },
-  {
-    name: "IU",
-    photo:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/IU_at_%27dlwlrma%27_concert_in_Jeju_on_January_5%2C_2019_%283%29.jpg/330px-IU_at_%27dlwlrma%27_concert_in_Jeju_on_January_5%2C_2019_%283%29.jpg",
-    scores: ["4", "5", "4", "5", "4", "5", "3", "3", "3", "3"]
   }
 ];
 
